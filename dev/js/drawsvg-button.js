2367897d839c02fb6b9b565bcaf76266299070d2
CatBatchelder <63326220+CatBatchelder@users.noreply.github.com>
2020-08-10 20:23:48 -0400

    trying to make the timeline play when clicked

--- dev/js/drawsvg-button.js
+++ dev/js/drawsvg-button.js
@@ -1,13 +1,10 @@
 
-console.log("drawsvg-working");
-
-$("#spiral").on("click", playUploadAnimation);
-
-//create the variable to make the click work
-var uploadIsPlaying = false;
+//console.log("drawsvg-working");
 
 //register plugin
 gsap.registerPlugin(DrawSVGPlugin);
+
+//$("#spiral").on("click", uploadAnimationTimeLine.play());
 
 //define the timeline
 var uploadAnimationTimeLine = gsap.timeline({
@@ -15,19 +12,40 @@ var uploadAnimationTimeLine = gsap.timeline({
 });
 
 //draw svg timeline
-uploadAnimationTimeLine.from("#spiral",{duration:5, drawSVG:"0%"});
-                        .to("checkmark", {opacity:1});
+uploadAnimationTimeLine.from("#spiral",{duration:5, drawSVG:"0%"})
+                        .to("check", {opacity:1});
+                        // .to("button", {opacity:1});
+
+//$("#spiral-animation").on("click", uploadAnimationTimeLine.play());
 
 
-
+//create the variable to make the click work
+// var uploadIsPlaying = false;
 
 //play the draw svg timeline
-if (uploadIsPlaying === false) {
 
-    uploadAnimationTimeLine.play();
+// function playUploadAnimation(){
+// if (uploadIsPlaying === false) {
 
-} else{
+//     uploadAnimationTimeLine.play();
 
-    uploadIsPlaying = false
+// } else{
 
-}
+//     uploadIsPlaying = false
+
+// }
+// }
+
+//$("#spiral").on("click", playUploadAnimation);
+
+$("spiral-animation").click(function(){
+    //if an animation is currently running reverse it.
+    if(uploadAnimationTimeLine) {
+        uploadAnimationTimeLine.play("exit");
+    }
+    
+    //set the currentAnimation to whatever this button's assigned animation is
+    uploadAnimationTimeLine = this.animation;
+    //play the animation
+    this.animation.play(0)
+  })
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,6 @@
-
+// @codekit-prepend "drawsvg-button.js";
 // @codekit-prepend "popup/upload-click.js";
 // @codekit-prepend "popup/popup.js";
-// @codekit-prepend "drawsvg-button.js";
+
 
 //console.log("does this work")
